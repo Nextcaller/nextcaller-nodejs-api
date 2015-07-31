13cdf9fb0a60e14491b09e2640ab19b081d991ba
yuri-sagalovich <yuriy.sahalovich@itechart-group.com>
2015-07-31 20:20:20 +0300

    Update tests in accordance to the NextCallerPlatformClient changes

--- test/test.js
+++ test/test.js
@@ -20,7 +20,8 @@ var should = require("should"),
     serialize = index.serialize,
     client = new (index.NextCallerClient)(username, password, true, apiVersion),
     platformClient = new (index.NextCallerPlatformClient)(username, password, true, apiVersion),
-    platformUsername = "test",
+    updateWithAccountHeader = index.updateWithPlatformAccountHeader,
+    accountId = "test",
     phoneResponseObject = {
         "records": [
             {
@@ -124,58 +125,60 @@ var should = require("should"),
     platformStatisticsResponseObject = {
         "object_list": [
             {
-                "username": "test",
+                "id": "test",
                 "first_name": "",
                 "last_name": "",
                 "company_name": "",
                 "email": "",
                 "number_of_operations": 3,
-                "successful_calls": {
-                    "201411": 3
+                "billed_operations": {
+                    "2014-11": 3
                 },
-                "total_calls": {
-                    "201411": 3
+                "total_operations": {
+                    "2014-11": 3
                 },
-                "created_time": "2014-11-13 06:07:19.836404",
-                "resource_uri": "/v2/platform_users/test/"
+                "object": "account",
+                "resource_uri": "/v2/accounts/test/"
             }
         ],
-       "page": 1,
+        "page": 1,
         "has_next": false,
         "total_pages": 1,
-        "total_platform_calls": {
+        "object": "page",
+        "total_platform_operations": {
             "2014-11": 3
         },
-        "successful_platform_calls": {
+        "billed_platform_operations": {
             "2014-11": 3
         }
     },
-    platformStatisticsByUserResponseObject = {
-        "username": "test",
+    platformStatisticsByAccountResponseObject = {
+        "id": "test",
         "first_name": "",
         "last_name": "",
         "company_name": "",
         "email": "",
         "number_of_operations": 3,
-        "successful_calls": {
+        "billed_operations": {
             "201411": 3
         },
-        "total_calls": {
+        "total_operations": {
             "201411": 3
         },
-        "resource_uri": "/v2/platform_users/test/"
+        "object": "account",
+        "resource_uri": "/v2/accounts/test/"
     },
-    platformUpdateUsernameJsonRequestExample = {
+    platformUpdateAccountJsonRequestExample = {
         "first_name": "Clark",
         "last_name": "Kent",
         "email": "test@test.com"
     },
-    platformUpdateUsernameWrongJsonRequestExample = {
+    platformUpdateAccountWrongJsonRequestExample = {
         "first_name": "Clark",
         "last_name": "Kent",
         "email": "XXXX"
     },
-    platformUpdateUserWrongResult = {
+    platformUpdateAccountWrongResult = {
         "error": {
             "message": "Validation Error",
             "code": "422",
@@ -214,7 +217,6 @@ var should = require("should"),
         "address": "7160 Sw Crestview Pl",
         "zip_code": 97008
     };
-
 
 
 describe("getByPhone with correct phone number", function () {
@@ -360,11 +362,10 @@ describe("getFraudLevel with correct phone number", function () {
 describe("platformClient getByPhone with correct phone number", function () {
     it("should return the correct response", function (done) {
         var phoneResponseObjectStr = JSON.stringify(phoneResponseObject);
-        nock("https://" + apiHostname)
-            .get("/" + apiVersion + "/records/?format=json&phone=" + phone +
-                "&platform_username=" + platformUsername)
+        nock("https://" + apiHostname, {reqheaders: updateWithAccountHeader(accountId)})
+            .get("/" + apiVersion + "/records/?format=json&phone=" + phone)
             .reply(200, phoneResponseObjectStr);
-        platformClient.getByPhone(phone, platformUsername, function (data, statusCode) {
+        platformClient.getByPhone(phone, accountId, function (data, statusCode) {
             statusCode.should.equal(200);
             data.records[0].phone[0].number.should.equal(phone.toString());
             data.records[0].id.should.equal(profileId);
@@ -376,13 +377,13 @@ describe("platformClient getByPhone with correct phone number", function () {
 
 describe("platformClient get platform statistics", function () {
     it("should return the correct response", function (done) {
-        var platformResponseResponseObjectStr = JSON.stringify(platformStatisticsResponseObject);
+        var platformStatisticsResponseObjectStr = JSON.stringify(platformStatisticsResponseObject);
         nock("https://" + apiHostname)
-            .get("/" + apiVersion + "/platform_users/?format=json&page=1")
-            .reply(200, platformResponseResponseObjectStr);
+            .get("/" + apiVersion + "/accounts/?format=json&page=1")
+            .reply(200, platformStatisticsResponseObjectStr);
         platformClient.getPlatformStatistics(1, function (data, statusCode) {
             statusCode.should.equal(200);
-            data.object_list[0].username.should.equal(platformUsername);
+            data.object_list[0].id.should.equal(accountId);
             data.object_list[0].number_of_operations.should.equal(3);
             data.page.should.equal(1);
             done();
@@ -391,16 +392,16 @@ describe("platformClient get platform statistics", function () {
 });
 
 
-describe("platformClient get platform statistics by user", function () {
+describe("platformClient get platform statistics by account", function () {
     it("should return the correct response", function (done) {
-        var platformResponseByUserResponseObjectStr = JSON.stringify(platformStatisticsByUserResponseObject),
-            path = "/" + apiVersion + "/platform_users/" + platformUsername + "/?format=json";
+        var platformResponseByAccountResponseObjectStr = JSON.stringify(platformStatisticsByAccountResponseObject),
+            path = "/" + apiVersion + "/accounts/" + accountId + "/?format=json";
         nock("https://" + apiHostname)
             .get(path)
-            .reply(200, platformResponseByUserResponseObjectStr);
-        platformClient.getPlatformUser(platformUsername, function (data, statusCode) {
+            .reply(200, platformResponseByAccountResponseObjectStr);
+        platformClient.getPlatformAccount(accountId, function (data, statusCode) {
             statusCode.should.equal(200);
-            data.username.should.equal(platformUsername);
+            data.id.should.equal(accountId);
             data.number_of_operations.should.equal(3);
             done();
         });
@@ -408,13 +409,13 @@ describe("platformClient get platform statistics by user", function () {
 });
 
 
-describe("platformClient update platform user", function () {
+describe("platformClient update platform account", function () {
     it("should return the correct response", function (done) {
-        var path = "/" + apiVersion + "/platform_users/" + platformUsername + "/?format=json";
+        var path = "/" + apiVersion + "/accounts/" + accountId + "/?format=json";
         nock("https://" + apiHostname)
             .post(path)
             .reply(204, "");
-        platformClient.updatePlatformUser(platformUsername, platformUpdateUsernameJsonRequestExample, function (data, statusCode) {
+        platformClient.updatePlatformAccount(platformUpdateAccountJsonRequestExample, accountId, function (data, statusCode) {
             statusCode.should.equal(204);
             data.should.equal("");
             done();
@@ -423,14 +424,14 @@ describe("platformClient update platform user", function () {
 });
 
 
-describe("platformClient update platform user with incorrect data", function () {
+describe("platformClient update platform account with incorrect data", function () {
     it("should return 400 response", function (done) {
-        var platformUpdateUserWrongResultStr = JSON.stringify(platformUpdateUserWrongResult),
-            path = "/" + apiVersion + "/platform_users/" + platformUsername + "/?format=json";
+        var platformUpdateAccountWrongResultStr = JSON.stringify(platformUpdateAccountWrongResult),
+            path = "/" + apiVersion + "/accounts/" + accountId + "/?format=json";
         nock("https://" + apiHostname)
             .post(path)
-            .reply(400, platformUpdateUserWrongResultStr);
-        platformClient.updatePlatformUser(platformUsername, platformUpdateUsernameWrongJsonRequestExample, null, function (data, statusCode) {
+            .reply(400, platformUpdateAccountWrongResultStr);
+        platformClient.updatePlatformAccount(platformUpdateAccountWrongJsonRequestExample, accountId, null, function (data, statusCode) {
             statusCode.should.equal(400);
             data.error.description.email[0].should.equal("Enter a valid email address.");
             done();
@@ -438,13 +439,14 @@ describe("platformClient update platform user with incorrect data", function () 
     });
 });
 
+
 describe("platformClient getFraudLevel with correct phone number", function () {
     it("should return the correct response", function (done) {
         var fraudResponseObjectStr = JSON.stringify(fraudGetLevelResult);
-        nock("https://" + apiHostname)
-            .get("/" + apiVersion + "/fraud/?format=json&phone=" + phone + "&platform_username=" + platformUsername)
+        nock("https://" + apiHostname, {reqheaders: updateWithAccountHeader({}, accountId)})
+            .get("/" + apiVersion + "/fraud/?format=json&phone=" + phone)
             .reply(200, fraudResponseObjectStr);
-        platformClient.getFraudLevel(phone, platformUsername, function (data, statusCode) {
+        platformClient.getFraudLevel(phone, accountId, function (data, statusCode) {
             statusCode.should.equal(200);
             data.spoofed.should.equal("false");
             data.fraud_risk.should.equal("low");
@@ -453,16 +455,16 @@ describe("platformClient getFraudLevel with correct phone number", function () {
     });
 });
 
+
 describe("PlatformClient getByAddressName with correct address", function () {
     it("should return the correct response", function (done) {
         var addressResponseObjectStr = JSON.stringify(phoneResponseObject),
-            address_data = correctAddressRequestObj;
-        address_data.format = "json";
-        address_data.platform_username = platformUsername;
-        nock("https://" + apiHostname)
-            .get("/" + apiVersion + "/records/" + serialize(address_data)).
+            request_data = correctAddressRequestObj;
+        request_data.format = "json";
+        nock("https://" + apiHostname, {reqheaders: updateWithAccountHeader({}, accountId)})
+            .get("/" + apiVersion + "/records/" + serialize(request_data)).
             reply(200, addressResponseObjectStr);
-        platformClient.getByAddressName(address_data, platformUsername, function (data, statusCode) {
+        platformClient.getByAddressName(request_data, accountId, function (data, statusCode) {
             statusCode.should.equal(200);
             data.records[0].phone[0].number.should.equal(phone.toString());
             data.records[0].id.should.equal(profileId);
@@ -475,13 +477,12 @@ describe("PlatformClient getByAddressName with correct address", function () {
 describe("PlatformClient getByAddressName with incorrect address data", function () {
     it("should return 400 error", function (done) {
         var adddressErrorObjectStr = JSON.stringify(wrongAddressResponseObj),
-            address_data = wrongAddressRequestObj;
-        address_data.format = "json";
-        address_data.platform_username = platformUsername;
-        nock("https://" + apiHostname)
-            .get("/" + apiVersion + "/records/" + serialize(address_data))
+            request_data = wrongAddressRequestObj;
+        request_data.format = "json";
+        nock("https://" + apiHostname, {reqheaders: updateWithAccountHeader({}, accountId)})
+            .get("/" + apiVersion + "/records/" + serialize(request_data))
             .reply(400, adddressErrorObjectStr);
-        platformClient.getByAddressName(address_data, platformUsername, null, function (data, statusCode) {
+        platformClient.getByAddressName(request_data, accountId, null, function (data, statusCode) {
             statusCode.should.equal(400);
             data.error.code.should.equal("422");
             done();
